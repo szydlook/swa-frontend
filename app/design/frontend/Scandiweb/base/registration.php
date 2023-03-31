d22df31efbfbac2e8688735130680d31add99410
szydlook <szydlook@gmail.com>
2023-03-31 20:19:35 +0000

    Task8 completed

--- app/design/frontend/Scandiweb/base/registration.php
+++ app/design/frontend/Scandiweb/base/registration.php
@@ -1,4 +1,7 @@
-<?php \Magento\Framework\Component\ComponentRegistrar::register(
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::THEME,
 	'frontend/Scandiweb/base',
 	__DIR__
